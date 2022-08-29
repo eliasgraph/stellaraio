8b8d144cad3d80a1ab3700c6e7a53ffc5c350ec6
oliethan <oliethan0000@gmail.com>
2022-08-29 15:36:41 +0100

    bug fixes

--- src/components/Blog/Posts.js
+++ src/components/Blog/Posts.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 function Posts({posts}) {
-  
+  const pushToBlogDetail =(item)=>{
+    window.location.href = `/blog/${item.id}`
+  }
   return (
     <>
       {posts.length && posts.map((post, key)=>(
-        <div key={key} className="side-posts mb-20px">
+        <div role={'button'} onClick={()=>{pushToBlogDetail(post)}} key={key} className="side-posts mb-20px">
         <h6>{post.title.rendered}</h6>
         <p className='mb-0'>Author: {post.acf.publisher}</p>
       </div>
