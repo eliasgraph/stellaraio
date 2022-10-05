1450f46c37521f99d3fa565bad0afba2b50e8d99
dotmantosh <dotuntosh@gmail.com>
2022-10-05 16:16:37 +0100

    google analytic seo.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* <Script src="/seo.js"/> */}
+        <Script src="/seo.js"/>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600;700&display=swap" rel="stylesheet"/>
@@ -13,9 +13,9 @@ export default function Document() {
       <body>
         <noscript>
           <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MMVSWXQ"
-          height="0" width="0" style={{display:'none', visibility:"hidden"}}>
-          </iframe>
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>
         </noscript>
+
         <Main />
         <NextScript />
       </body>
