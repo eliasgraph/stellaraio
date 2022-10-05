2d09d0b10fe158835f664a7288a4ded754c14c59
dotmantosh <dotuntosh@gmail.com>
2022-10-05 18:03:37 +0100

    google analytics script

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* <Script src="/seo.js"/> */}
+        <Script strategy='afterInteractive' dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MMVSWXQ');`}} ></Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600;700&display=swap" rel="stylesheet"/>
