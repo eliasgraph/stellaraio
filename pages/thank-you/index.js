3f1dfe7863ac787717ef08b95e7941dfc7d164bb
dotmantosh <dotuntosh@gmail.com>
2023-03-27 14:08:26 +0100

    thankyou page

--- pages/thank-you/index.js
+++ pages/thank-you/index.js
@@ -1,6 +1,6 @@
-import CardGradient from '../../components/Generals/CardGradient'
-import tank from '../../assets/imgs/thankyou/Mockupss 1.png'
-import glowring from '../../assets/imgs/Path 5.png'
+import CardGradient from "../../components/Generals/CardGradient";
+import tank from "../../assets/imgs/thankyou/Mockupss 1.png";
+import glowring from "../../assets/imgs/Path 5.png";
 
 function ThankYou() {
   return (
@@ -8,21 +8,22 @@ function ThankYou() {
       <section className="thank-you">
         <CardGradient>
           <div className="thank-you-wrapper">
-            <h1 className='text-center'>Thank you for taking interest in <br /> <span className="color-purple-2">Stellar AIO.</span></h1>
-            <p className="text-center">You will receive an email if you are selected.</p>
+            <h1 className="text-center">
+              Thanks for signing up! Please check your inbox to confirm your
+              request.
+            </h1>
 
             <div className="thank-you-img">
-              <img className='thank-img' src={tank} alt="" />
-              <img className='thank-glowring-1' src={glowring} alt="" />
-              <img className='thank-glowring-2' src={glowring} alt="" />
-              <img className='thank-glowring-3' src={glowring} alt="" />
+              <img className="thank-img" src={tank} alt="" />
+              <img className="thank-glowring-1" src={glowring} alt="" />
+              <img className="thank-glowring-2" src={glowring} alt="" />
+              <img className="thank-glowring-3" src={glowring} alt="" />
             </div>
           </div>
-
         </CardGradient>
       </section>
     </>
-  )
+  );
 }
 
-export default ThankYou
+export default ThankYou;
