52a41d79dc4fdb4aefa177b8c627df62387173b5
Kristián Hvasta <rebelvole951@gmail.com>
2023-12-06 17:55:36 +0100

    Added tmx collector

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,38 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600;700&display=swap" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        ></link>
         <link rel="icon" type="image/x-icon" href="/favicon.png" />
       </Head>
       <body>
         <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MMVSWXQ"
-          height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MMVSWXQ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
         </noscript>
 
         <Main />
         <NextScript />
+        <script src="/c.js"></script>
       </body>
     </Html>
-  )
+  );
 }
